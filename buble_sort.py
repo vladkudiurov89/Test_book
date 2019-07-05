1abff6b011e1b67e914903ecdb3af3bb21366c20
Vlad <49581807+vladkudiurov89@users.noreply.github.com>
2019-07-05 18:28:04 +0300

    Add files via upload
    OK

---
+++ buble_sort.py
@@ -0,0 +1,56 @@
+# # buble sort with for
+from random import randint
+#
+# N = 40
+# a = []
+# for i in range(N):
+#     a.append(randint(1, 99))
+# print(a)
+#
+# for i in range(N-1):
+#     for j in range(N-i-1):
+#         if a[j]>a[j+1]:
+#             a[j], a[j+1] = a[j+1], a[j]
+#
+# print(a)
+
+
+# buble sort with while
+
+# N = 40
+# a = []
+# for i in range(N):
+#     a.append(randint(1, 99))
+# print(a)
+#
+# i = 0
+# while i < N - 1:
+#     j = 0
+#     while j < N - 1 -i:
+#         if a[j]> a[j+1]:
+#             a[j], a[j+1] = a[j+1], a[j]
+#         j += 1
+#     i += 1
+# print(a)
+
+# function for buble sort
+
+# def buble (array):
+#     for i in range(N-1):
+#         for j in range (N - 1):
+#             if array[j] > array[j+1]:
+#                 buff = array[j]
+#                 array[j] = array [j+1]
+#                 array [j+1] = buff
+# N = 40
+# a = []
+# for i in range(N):
+#     a.append(randint(1, 99))
+#
+# print(a)
+# buble(a)
+# print(a)
+
+
+
+
